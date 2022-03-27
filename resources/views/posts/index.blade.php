b84ad29d3c7bac73542af0710ea69c381e672851
Cristian Torres <cristiant@gmail.com>
2022-03-27 01:43:59 -0300

    navegacion categoria-nombre-tags

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,14 +7,14 @@
                     <div class="w-full h-full px-8 flex flex-col justify-center">
                         <div class="">
                             @foreach ($post->tags as $tag)
-                                <a href="#" class="inline-block px-3 h-6 bg-{{$tag->color}}-600 rounded-full">{{ $tag->name}}</a>
+                                <a href="{{route('posts.tag', $tag)}}" class="inline-block px-3 h-6 bg-{{$tag->color}}-600 rounded-full">{{ $tag->name}}</a>
                             @endforeach
                             
                         </div>
 
                         <div class="text-4x1 text-white leading-8 font-bold">
-                            <h1 class="text-4xl text-white lading-8 font-bold">
-                                <a href="#">{{$post->name}}</a>
+                            <h1 class="text-4xl text-white lading-8 font-bold mt-2">
+                                <a href="{{route('posts.show', $post)}}">{{$post->name}}</a>
                             </h1>
                         </div>
                     </div>
