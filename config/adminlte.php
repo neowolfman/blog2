a8425717706ffbdb0eb41bfa35fc90e8b73172ce
Cristian Torres <cristiant@gmail.com>
2022-03-27 23:00:53 -0300

    Instalacion de Plugins AdminLTE

--- config/adminlte.php
+++ config/adminlte.php
@@ -188,7 +188,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => '/',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
@@ -247,9 +247,9 @@ return [
             'can'  => 'manage-blog',
         ],
         [
-            'text'        => 'pages',
+            'text'        => 'Dashboard',
             'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
+            'icon'        => 'fas fa-tachometer-alt',
             'label'       => 4,
             'label_color' => 'success',
         ],
